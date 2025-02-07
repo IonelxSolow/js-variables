c24b57888a99a4287cf183a567b807fe37569d11
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-07 16:20:19 +0100

    fix 2

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const numOne = 8; // NON MODIFICARE QUESTA RIGA
-const numTwo = 16; // NON MODIFICARE QUESTA RIGA
-const numThree = 32; // NON MODIFICARE QUESTA RIGA
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+const name = "Ionel"
+console.log(name);
 
-// Sostituisci NaN di seguito utilizzando
-// gli operatori numerici di Javascript
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
 
-// 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = 8 + 16;
-console.log(numOnePlusNumTwo); // Output: 24
+const number = 1
+const text = "textxt"
+const bool = false
 
-// 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = 32 * 16;
-console.log(numThreeTimesNumTwo); // Output: 512
-
-// 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = 32 / 8;
-console.log(numThreeDividedByNumOne); // Output: 4
-
-// 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = 32 - 8;
-console.log(numThreeMinusNumOne); // Output: 24
-
-// 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = numOne + numTwo + numThree;
-console.log(sum); // Output: 56
-
-// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = (numOne + numTwo + numThree) / numOne;
-console.log(numBytes); // Output: 7
+console.log(number, text, bool);
 
 
-//
+
+
+
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = firstName + " " + lastName;
+console.log(fullName); // Output: "Jane Smith"
+
+
+
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(numberOfLettersInAlphabet); // Output: 26
